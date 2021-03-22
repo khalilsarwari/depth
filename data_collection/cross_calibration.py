952cab14e7c79a0b54ddb18ff72b682cbc902eca
khalilsarwari <khalil.sarwari@berkeley.edu>
2021-03-22 16:30:49 -0700

    starting old school cross calib

--- data_collection/cross_calibration.py
+++ data_collection/cross_calibration.py
@@ -10,22 +10,24 @@ from sensor_msgs import point_cloud2
 class CrossCalibrationNode(object):
     def __init__(self):
         self.bridge = CvBridge()
+        self.cc_pub = rospy.Publisher('cc', Image, queue_size=10)
         image_sub = Subscriber('camera/image', Image)
         pcl_sub = Subscriber('/livox/lidar',  PointCloud2)
         ts = ApproximateTimeSynchronizer([image_sub, pcl_sub], queue_size=10, slop=0.05)
         ts.registerCallback(self.callback)
 
     def callback(self, image, cloud):
-        print(image.header.stamp)
+        # print(image.header.stamp)
         cv_image = self.bridge.imgmsg_to_cv2(image, "bgr8")
-        print("Camera", image.header.stamp, image.header.seq)
+        # print("Camera", image.header.stamp, image.header.seq)
         points_list = []
 
         for p in point_cloud2.read_points(cloud, skip_nans=True):
             points_list.append([p[0],p[1], p[2], p[3]])
 
         pcl_data = np.array(points_list) # XYZI
-        print("LIDAR", cloud.header.stamp, pcl_data.shape)
+        # print("LIDAR", cloud.header.stamp, pcl_data.shape)
+        self.cc_pub.publish(self.bridge.cv2_to_imgmsg(cv_image))
 
 if __name__ == '__main__':
     rospy.init_node("cross_calibration_node", anonymous=True)
